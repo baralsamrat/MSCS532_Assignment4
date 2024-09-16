570050f26ad7420c57ae2b1d8a6fcda239c4b669
Samrat Baral <sbaral30064@ucumberlands.edu>
2024-09-15 19:22:57 -0500

    Update part2.py
    Added functionality insert,search,swap, and more

--- part2.py
+++ part2.py
@@ -16,24 +16,49 @@ class PriorityQueue:
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
     def insert(self, task):
-        return
-    
+        self.heap.append(task)
+        self._sift_up(len(self.heap) - 1)
+
     def extract_min(self):
-        return
+        if self.is_empty():
+            return None
+        self._swap(0, len(self.heap) - 1)
+        min_task = self.heap.pop()
+        self._sift_down(0)
+        return min_task
 
     def decrease_priority(self, task_index, new_priority):
-        return
-        
+        self.heap[task_index].priority = new_priority
+        self._sift_up(task_index)
+
     def is_empty(self):
-        return 
-        
+        return len(self.heap) == 0
+
     def _sift_up(self, idx):
-        return
-        
+        parent = (idx - 1) // 2
+        if parent >= 0 and self.heap[idx] < self.heap[parent]:
+            self._swap(idx, parent)
+            self._sift_up(parent)
+
     def _sift_down(self, idx):
-        return 
-        
-# Example 
+        left = 2 * idx + 1
+        right = 2 * idx + 2
+        smallest = idx
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        if smallest != idx:
+            self._swap(idx, smallest)
+            self._sift_down(smallest)
+
+    
+# Example usage
+pq = PriorityQueue()
+tasks = [Task(1, 3, 0, 5), Task(2, 1, 1, 4), Task(3, 2, 2, 6)]
+for task in tasks:
+    pq.insert(task)
 
 print("Tasks sorted by priority:")
 while not pq.is_empty():
