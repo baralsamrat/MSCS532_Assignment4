63b9b8139351fbeeb1db25baaf11901634f1f3c8
Samrat Baral <sbaral30064@ucumberlands.edu>
2024-09-15 19:32:38 -0500

    Create simulation.py
    Foundation for simulation to work

---
+++ simulation.py
@@ -0,0 +1,12 @@
+def scheduling_simulation():
+    # import Priority Queue
+    pq = PriorityQueue()
+
+    # Create tasks with task_id, priority, arrival_time, deadline
+    
+    # Insert tasks into the priority queue
+    
+    # Extract tasks based on priority
+    
+# Run scheduling simulation
+scheduling_simulation()
