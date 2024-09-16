f36f8a93d1ed233694553d81b6d312f0704c718a
Samrat Baral <sbaral30064@ucumberlands.edu>
2024-09-15 19:16:01 -0500

    Update Heapsort.py
    Added Heapify, and tested it

--- Heapsort.py
+++ Heapsort.py
@@ -1,8 +1,24 @@
-# Heapsort implementation in Python
+# Heapsort implementation
 
-def heapify(arr, n, i):
-    # Implement heapify
-  
+def heapify_arr(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1     # left child
+    right = 2 * i + 2    # right child
+
+    # If left child exists and is greater than root
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    # If right child exists and is greater than the largest so far
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    # If largest is not root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # Swap
+        # Recursively heapify the affected sub-tree
+        heapify(arr, n, largest)
+
 def heapsort(arr):
     n = len(arr)
 
@@ -12,7 +28,10 @@ def heapsort(arr):
 
     # Extract elements one by one from the heap
     for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]   # Swaping
-        heapify(arr, i, 0)
+        arr[i], arr[0] = arr[0], arr[i]   # Swap here
+        heapify_arr(arr, i, 0)
 
-# Example usage
+# Test/Example usage
+arr = [12, 11, 13, 5, 6, 7]
+heapsort(arr)
+print("Sorted array is:", arr)
