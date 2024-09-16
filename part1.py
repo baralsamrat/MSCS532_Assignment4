6ae1797c31cb8172991435fd857bf06f48293277
Samrat Baral <sbaral30064@ucumberlands.edu>
2024-09-15 19:56:37 -0500

    Update part1.py
    Check the sorting on heapify

--- part1.py
+++ part1.py
@@ -10,7 +10,9 @@ def iterative_heapify(arr, n, i):
             largest = right
 
         if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
+            arr[i], arr[largest] = arr[largest], arr[i] 
+            print(f"Heapified {arr}")  # Debug print
+            iterative_heapify(arr, n, largest)
             i = largest  # Continue heapifying down the tree
         else:
             break
@@ -24,7 +26,19 @@ def heapsort(arr):
     # Extract elements one by one from the heap
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
+         print(f"Swapped {arr}")  # Debug print
         iterative_heapify(arr, i, 0)
+
+# def heapsort(arr):
+#     n = len(arr)
+#     for i in range(n // 2 - 1, -1, -1):
+#         heapify(arr, n, i)
+    
+#     for i in range(n - 1, 0, -1):
+#         arr[i], arr[0] = arr[0], arr[i]
+#         print(f"Swapped {arr}")  # Debug print
+#         heapify(arr, i, 0)
+
 
 # Example usage
 arr = [12, 11, 13, 5, 6, 7]
